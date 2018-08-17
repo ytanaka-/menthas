66915fa475192219c4b3340a0d5fa359b35d9b7a
ytanaka <y.matsuura000@gmail.com>
2018-08-18 00:01:38 +0900

    fix indicator css

--- src/frontend/components/Navigation.vue
+++ src/frontend/components/Navigation.vue
@@ -2,9 +2,16 @@
   <div class="navigation">
     <ul class="navigation-container">
       <li class="channel-section" v-for="channel in channels" v-bind:key="channel.name">
-        <router-link v-bind:to="channel.name">
-          <span class="navigation-channel-text">{{ channel.title }}</span>
-        </router-link>
+        <template v-if="channel.name == 'all'">
+          <router-link to="/" exact>
+            <span class="navigation-channel-text">{{ channel.title }}</span>
+          </router-link>
+        </template>
+        <template v-else>
+          <router-link v-bind:to="channel.name">
+            <span class="navigation-channel-text">{{ channel.title }}</span>
+          </router-link>
+        </template>
       </li>
     </ul>
   </div>
@@ -28,7 +35,6 @@ export default {
 <style lang="stylus">
 .navigation
   width 100%
-  height 35px
   border-bottom 1px solid #ccc
   position sticky
   position -webkit-sticky
@@ -55,7 +61,7 @@ ul
   min-width 50px
   a
     display block
-    height 33px
+    height 35px
     padding 0 5px
     text-align center
     line-height 33px
@@ -67,10 +73,13 @@ ul
     color #1C5D99
     text-decoration underline
   .router-link-active
+    box-sizing border-box
     border-bottom 2px solid #222
 
 @media screen and (max-width: 480px)
-  .navigation
-    height 45px
+  .channel-section
+    a
+      height 42px
+      line-height 40px
 
 </style>
