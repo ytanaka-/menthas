1f57637ae34f2ee517356eae39a97240422b0d4d
ytanaka <y.matsuura000@gmail.com>
2018-08-29 02:30:46 +0900

    update diversified news choice using mmr

--- src/backend/model/page-service.js
+++ src/backend/model/page-service.js
@@ -2,20 +2,16 @@ const Page = require("./page")
 const moment = require('moment')
 const config = require('config')
 const SELECTION_SIZE = config.selection_size
-const MMR_REL_WEIGHT = 0.75
+const MMR_REL_WEIGHT = config.mmr_rel_weight
 
 class PageService {
 
   curatedNewsSelect(threshold, size){
-    const _pages = []
     return new Promise((resolve, reject) => {
       Page.findCuratedNews(threshold, SELECTION_SIZE)
       .then((pages)=>{
-        const selectionIndexes = this._diversifiedSelect(pages, size, ["all"])
-        selectionIndexes.forEach((index)=>{
-          _pages.push(pages[index])
-        })
-        resolve(_pages)
+        const selectionPages = this._diversifiedSelect(pages, size, ["all"])
+        resolve(selectionPages)
       }).catch((err)=>{
         reject(err);
       })
@@ -23,15 +19,11 @@ class PageService {
   }
 
   curatedNewsSelectByCategory(channelName, categories, threshold, size){
-    const _pages = []
     return new Promise((resolve, reject) => {
       Page.findCuratedNewsByCategory(categories, threshold, SELECTION_SIZE)
       .then((pages)=>{
-        const selectionIndexes = this._diversifiedSelect(pages, size, categories)
-        selectionIndexes.forEach((index)=>{
-          _pages.push(pages[index])
-        })
-        resolve(_pages)
+        const selectionPages = this._diversifiedSelect(pages, size, categories)
+        resolve(selectionPages)
       }).catch((err)=>{
         reject(err);
       })
@@ -40,7 +32,8 @@ class PageService {
 
   // 指定された件数までMMRを使ってindexを登録していく
   _diversifiedSelect(pages, size, categories) {
-    const selectedIndexes = []
+    const selectedPages = []
+    const selectedPageUnions = []
     const now = moment()
     // categoriesはmongooseのidの配列なので文字列にする
     const _categories = []
@@ -64,22 +57,46 @@ class PageService {
             }
           }
         })
+        // 類似度計測用に使う要素
+        const union = [hostName, category.name].concat(category.curator)
         if (category.score > 5) {
           category.score = 5
         }
         // 時間経過からrelを算出
         const rel = category.score / ((diff + 2) ^ 1.5)
-        // hostやcategory, curatorから類似度を算出
-        const sim = 0
-        const mmrScore = MMR_REL_WEIGHT * rel + (1 - MMR_REL_WEIGHT) * sim
-        if (!selectedIndexes.includes(index) && (max.index == undefined || mmrScore > max.score)) {
+        // 類似度を算出
+        const sim = this._maxSim(union, selectedPageUnions)
+        const mmrScore = MMR_REL_WEIGHT * rel - (1 - MMR_REL_WEIGHT) * sim
+        if (max.index == undefined || mmrScore > max.score) {
           max.index = index
           max.score = mmrScore
+          max.union = union
         }
       })
-      selectedIndexes.push(max.index)
+      selectedPages.push(pages[max.index])
+      selectedPageUnions.push(max.union)
+      pages.splice(max.index, 1)
     }
-    return selectedIndexes
+    return selectedPages
+  }
+
+  _maxSim(union, selectedUnions){
+    let max = 0
+    selectedUnions.forEach((_union) => {
+      let intersection = 0
+      union.forEach((u1)=>{
+        _union.forEach((u2)=>{
+          if(u1 == u2) {
+            intersection++
+          }
+        })
+      })
+      const sim = intersection / (union.length + _union.length - intersection)
+      if(sim > max){
+        max = sim
+      }
+    })
+    return max
   }
 
 }
