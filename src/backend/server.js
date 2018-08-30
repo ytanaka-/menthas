b518cd25c1406035c3c262476358f3149b2a986a
ytanaka <y.matsuura000@gmail.com>
2018-08-31 00:25:39 +0900

    update meta tag and link files

--- src/backend/server.js
+++ src/backend/server.js
@@ -15,14 +15,13 @@ const server = express();
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
 server.use(helmet());
+server.use(express.static(path.join(__dirname, '../../public')));
 server.use('/', require('./controller/index'));
 server.use('/api/', require('./controller/api'));
 
 server.set('views', path.join(__dirname, 'view'));
 server.set('view engine', 'jade');
 server.disable('x-powered-by');
-server.use(express.static(path.join(__dirname, '../../public')));
-server.use(favicon(path.join(__dirname, '../../public/images/favicon.ico')));
 
 server.listen(port, (err) => {
   if (err) throw err
