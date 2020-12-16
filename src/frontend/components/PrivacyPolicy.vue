dd812a9b300d7df87fc38f88f00c1fde445daf4a
ytanaka <y.matsuura000@gmail.com>
2020-12-17 07:02:23 +0900

    fix: privacy-policy css

--- src/frontend/components/PrivacyPolicy.vue
+++ src/frontend/components/PrivacyPolicy.vue
@@ -1,21 +1,23 @@
 <template>
-  <div class="privacy-policy-container">
-    <h2 class="title">プライバシーポリシー</h2>
-    <p class="main">Menthasでは以下に記載するプライバシーポリシーに基づき、Google Analyticsで得られた情報を使用しています</p>
-    <p class="bold">Google Analyticsの利用目的</p>
-    <ul>
-      <li>ユーザビリティやアルゴリズムの改善のために統計情報として、ユーザ数やどんなニュースが多くクリックされているかといった分析を行うために使用します</li>
-      <li>Google Analyticsがどのように情報を収集するかは以下の公式記事を参照してください</li>
-      <li>
-        <a href='https://policies.google.com/privacy?hl=ja' target='_blank' class='url'>https://policies.google.com/privacy?hl=ja</a>
-      </li>
-    </ul>
-    <p class="bold">その他/取り扱いについて</p>
-    <ul>
-      <li>Menthasにはアカウントは存在しないため、Google Analyticsを用いても個人を特定することはできません</li>
-      <li>Google Analyticsにより収集された情報は、Google社のプライバシーポリシーに基づいて管理されます</li>
-      <li>またGoogle Analyticsで収集した情報を、法令に基づく何らかの特別な場合を除いて、第三者に提供することはしないものとします</li>
-    </ul>
+  <div class="privacy-policy-wrap">
+    <div class="privacy-policy-container">
+      <h2 class="title">プライバシーポリシー</h2>
+      <p class="main">Menthasでは以下に記載するプライバシーポリシーに基づき、Google Analyticsで得られた情報を使用しています</p>
+      <p class="bold">Google Analyticsの利用目的</p>
+      <ul>
+        <li>ユーザビリティやアルゴリズムの改善のために統計情報として、ユーザ数やどんなニュースが多くクリックされているかといった分析を行うために使用します</li>
+        <li>Google Analyticsがどのように情報を収集するかは以下の公式記事を参照してください</li>
+        <li>
+          <a href='https://policies.google.com/privacy?hl=ja' target='_blank' class='url'>https://policies.google.com/privacy?hl=ja</a>
+        </li>
+      </ul>
+      <p class="bold">その他/取り扱いについて</p>
+      <ul>
+        <li>Menthasにはアカウントは存在しないため、Google Analyticsを用いても個人を特定することはできません</li>
+        <li>Google Analyticsにより収集された情報は、Google社のプライバシーポリシーに基づいて管理されます</li>
+        <li>またGoogle Analyticsで収集した情報を、法令に基づく何らかの特別な場合を除いて、第三者に提供することはしないものとします</li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -30,13 +32,14 @@ export default {
 </script>
 
 <style lang="stylus">
-.privacy-policy-container
+.privacy-policy-wrap
   width 650px
   height 100%
   margin 0 auto
-  padding 12px
   font-size 13px
-  
+
+.privacy-policy-container
+  padding 12px
   .title
     padding 12px 0
   .main
@@ -53,5 +56,9 @@ export default {
     font-size 14px
     padding-top 12px
     padding-bottom 4px
+
+@media screen and (max-width: 650px)
+  .privacy-policy-wrap
+    width 100%
 
 </style>
