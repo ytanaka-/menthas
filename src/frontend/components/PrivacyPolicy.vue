0960a287bc7fd0f41145c4d7756bd53787c94894
ytanaka <y.matsuura000@gmail.com>
2020-12-17 23:33:19 +0900

    fix: privacy policyをより正確なものに修正

--- src/frontend/components/PrivacyPolicy.vue
+++ src/frontend/components/PrivacyPolicy.vue
@@ -6,15 +6,18 @@
       <p class="bold">Google Analyticsの利用目的</p>
       <ul>
         <li>ユーザビリティやアルゴリズムの改善のために統計情報として、ユーザ数やどんなニュースが多くクリックされているかといった分析を行うために使用します</li>
-        <li>Google Analyticsがどのように情報を収集するかは以下の公式記事を参照してください</li>
+        <li>Google AnalyticsはCookieを使用します。その詳細については以下の公式記事を参照してください</li>
         <li>
-          <a href='https://policies.google.com/privacy?hl=ja' target='_blank' class='url'>https://policies.google.com/privacy?hl=ja</a>
+          <a href='https://developers.google.com/analytics/devguides/collection/analyticsjs/cookie-usage?hl=ja' target='_blank' class='url'>https://developers.google.com/analytics/devguides/collection/analyticsjs/cookie-usage?hl=ja</a>
         </li>
       </ul>
       <p class="bold">その他/取り扱いについて</p>
       <ul>
-        <li>Menthasにはアカウントは存在しないため、Google Analyticsを用いても個人を特定することはできません</li>
-        <li>Google Analyticsにより収集された情報は、Google社のプライバシーポリシーに基づいて管理されます</li>
+        <li>Menthasにはユーザアカウントは存在しないため、Google Analyticsを用いても個人を特定することはできません</li>
+        <li>Google Analyticsにより収集された情報は、以下のGoogle社が発行するプライバシーポリシーに基づいて管理されます</li>
+        <li>
+          <a href='https://policies.google.com/technologies/partner-sites?hl=ja' target='_blank' class='url'>https://policies.google.com/technologies/partner-sites?hl=ja</a>
+        </li>
         <li>またGoogle Analyticsで収集した情報を、法令に基づく何らかの特別な場合を除いて、第三者に提供することはしないものとします</li>
       </ul>
     </div>
@@ -56,6 +59,9 @@ export default {
     font-size 14px
     padding-top 12px
     padding-bottom 4px
+
+  a
+    word-break break-all
 
 @media screen and (max-width: 650px)
   .privacy-policy-wrap
