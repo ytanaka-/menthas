13eacf377eaea5eedcc20d9c8bc95de009b88c75
ytanaka <y.matsuura000@gmail.com>
2022-04-09 03:54:40 +0900

    fix: react移行

--- src/frontend/app.js
+++ src/frontend/app.js
@@ -1,13 +1,6 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
 import App from './components/App.jsx';
-import NewsList from './components/NewsList.jsx';
-import PrivacyPolicy from './components/PrivacyPolicy.jsx';
 import "./css/app.css";
 
 /*
@@ -29,12 +22,4 @@ new Vue({
 
 const container = document.getElementById('app');
 const root = createRoot(container);
-root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="privacy_policy" element={<PrivacyPolicy />} />
-      <Route path=":channel" element={<NewsList />} />
-    </Routes>
-  </BrowserRouter>
-);
+root.render(<App />);
--- src/frontend/components/App.jsx
+++ src/frontend/components/App.jsx
@@ -1,18 +1,29 @@
-import React, { useState, useRef, useEffect } from "react";
+import React from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import Header from './Header.jsx';
 import Navigation from './Navigation.jsx';
+import NewsList from './NewsList.jsx';
+import PrivacyPolicy from './PrivacyPolicy.jsx';
 import Footer from './Footer.jsx';
 
 const App = () => {
   return (
-    <>
+    <BrowserRouter>
       <div className="app">
         <Header />
         <Navigation />
-        
+        <Routes>
+          <Route path="privacy_policy" element={<PrivacyPolicy />} />
+          <Route path=":channel" element={<NewsList />} />
+          <Route index element={<NewsList />} />
+        </Routes>
         <Footer />
       </div>
-    </>
+    </BrowserRouter>
   );
 }
 
