5cd104617073b7c7b87bd1af31443ce2a69b2044
ytanaka <y.matsuura000@gmail.com>
2024-07-01 14:36:12 +0900

    update: version & config

--- src/backend/controller/index.js
+++ src/backend/controller/index.js
@@ -7,7 +7,7 @@ const CURATED_THRESHOLD = config.curated_threshold;
 const TOP_CURATED_THRESHOLD = config.top_threshold;
 const RSS_SIZE = config.rss_size;
 const RSS = require("rss");
-const GAID = config.ga_id;
+const GAID = process.env.GA_ID || config.ga_id;
 
 router.get("/", (req, res) => {
   res.render("index", { ga: GAID });
